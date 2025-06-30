54cb00b2b424100827f4ae02f64f173a42b896ad
Mathias <math.kuni123@gmail.com>
2025-06-29 23:45:47 -0500

    feat: Add variable declaration and some examples

---
+++ declaracion-variables.js
@@ -0,0 +1,18 @@
+//metodo antiguo
+var miNumero1 = 15;
+console.log(miNumero1);
+
+//metodo de ahora 
+let miNumero2 = 20;
+console.log(miNumero2);
+
+let numeroBingo1 = 148;
+console.log(numeroBingo1);
+
+//CamelCase
+let cumpleMathias = 3;
+console.log(cumpleMathias);
+
+//PascalCase
+let CumpleDeNico = 19;
+console.log(CumpleDeNico);
