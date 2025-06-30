e9173d085831d96149bed875e806fe2c2c846320
Mathias <math.kuni123@gmail.com>
2025-06-29 23:50:22 -0500

    feat: add the method typof in the variable to determinate the type

--- tipos-de-datos.js
+++ tipos-de-datos.js
@@ -14,30 +14,38 @@
 
  Undefined: Ausencia de valor
  Ej: undefined
+
+ Typeof: Determinar que tipo de dato esta usando
  */
 
  //Valor Number
  let miEntero = 10;
  console.log(miEntero);
+ console.log(typeof miEntero);
 
  //Valor Flotante
  let miFlotante = 7.0;
  console.log(miFlotante);
+ console.log(typeof miFlotante);
 
  //Valor String
  let miCadena = "Hola soy cadena";
  console.log(miCadena);
+ console.log(typeof miCadena);
 
  //Valor Boolean
  let miBoolean = true;
  console.log(miBoolean);
+ console.log(typeof miBoolean);
  let miBoolean2 = false;
  console.log(miBoolean2);
-
+ console.log(typeof miBoolean2);
  //Valor Null
  let miNull = null
  console.log(miNull);
+ console.log(typeof miNull);
 
  //Valor Undefined
  let miUndefined = undefined
  console.log(miUndefined);
+ console.log(typeof miUndefined);
