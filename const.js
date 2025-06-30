c3eaef4d923c61a4c58fb613d55c94d317d4f543
Mathias <math.kuni123@gmail.com>
2025-06-29 23:57:49 -0500

    feat: add constants and examples for immutable variable usage

---
+++ const.js
@@ -0,0 +1,18 @@
+//Constante
+
+/*
+Const: Variable una vez definida con su valor, no se puede modificar
+*/
+
+const MI_CONSTANTE = 10;
+//MI_CONSTANTE = 20 no se puede cambiar el valor de una constante
+
+console.log(MI_CONSTANTE);
+
+//Constante ya definida
+console.log(Math.PI)
+
+//Constante segundos por minuto
+const SEGUNDOS_SEGUNDOS_POR_MINUTO = 60;
+console.log(SEGUNDOS_SEGUNDOS_POR_MINUTO);
+
