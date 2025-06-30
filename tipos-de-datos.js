ed316087b4ca119faac8980acb275eccc8901e7d
Mathias <math.kuni123@gmail.com>
2025-06-29 23:46:29 -0500

    feat: add types of definition data

---
+++ tipos-de-datos.js
@@ -0,0 +1,43 @@
+ /* Tipos de Datos
+
+ Number : Almacena valores numéricos
+ Ej: 10,40,-10,-20,etc
+
+ String: Almacena cadenas de caracteres
+ Ej: "Hola", "Chau", "Test"
+
+ Boolean: Almacena un valor lógico
+ Ej: True o False
+
+ Null: Ausencia de la referencia de un objeto
+ Ej: null
+
+ Undefined: Ausencia de valor
+ Ej: undefined
+ */
+
+ //Valor Number
+ let miEntero = 10;
+ console.log(miEntero);
+
+ //Valor Flotante
+ let miFlotante = 7.0;
+ console.log(miFlotante);
+
+ //Valor String
+ let miCadena = "Hola soy cadena";
+ console.log(miCadena);
+
+ //Valor Boolean
+ let miBoolean = true;
+ console.log(miBoolean);
+ let miBoolean2 = false;
+ console.log(miBoolean2);
+
+ //Valor Null
+ let miNull = null
+ console.log(miNull);
+
+ //Valor Undefined
+ let miUndefined = undefined
+ console.log(miUndefined);
