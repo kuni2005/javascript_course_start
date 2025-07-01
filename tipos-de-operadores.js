154641d67181050b318c2b28da0381f1840ef3e4
Mathias <math.kuni123@gmail.com>
2025-07-01 00:32:35 -0500

    feat: add arithmetic operators examples and explanations

---
+++ tipos-de-operadores.js
@@ -0,0 +1,40 @@
+/* Tipos de Operadores
+
+Aritméticos:
+
++ Suma
+- Resta
+* Multiplicar
+/ Dividir
+% Modulo (Residuo Division)
+** Potencia (Exponente)
+++ Incremento
+-- Decremento
+*/
+
+let a,b,c,d,e,f;
+
+//Suma
+a = 3 + 4; 
+console.log(a);
+
+//Resta
+b = 10-7;
+console.log(b);
+
+//Multiplicar
+c = 10 * 20;
+console.log(c);
+
+//Dividir
+d = 100/2;
+console.log(d);
+
+//Modulo
+f = 9 % 2;
+console.log(f);
+
+// Potencia
+e = 2 ** 3; //2 a la 8
+console.log(e)
+
