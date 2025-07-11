8899cab95ffaa05051c82ba2c48b1df00e7c995e
Mathias <math.kuni123@gmail.com>
2025-07-10 23:11:26 -0500

    feat: add assignment operators examples and explanations

---
+++ operadores-asignacion.js
@@ -0,0 +1,38 @@
+/* Operadores de Asignación
+
+= Asignación Ej: x = 5;
+
++= Asignación compuesto de suma
+Ej: x += 3; //x = x + 3
+
+*/
+
+let miNumero = 10;
+console.log(miNumero);
+
+miNumero = 30
+console.log(miNumero);
+
+//Operador +=
+miNumero += 5;
+console.log(miNumero);
+
+//Operador -=
+miNumero -= 3;
+console.log(miNumero);
+
+//Operador *=
+miNumero *= 2;
+console.log(miNumero);
+
+//Operador /=
+miNumero /= 4;
+console.log(miNumero);
+
+//Operador %=
+miNumero %= 2;
+console.log(miNumero);
+
+miNumero = 20;
+miNumero **= 2;
+console.log(miNumero);
---
+++ operadores.js
@@ -0,0 +1,27 @@
+//Operadores Incremento / Decremento
+let a,b,c;
+a = 0;
+
+// Pre-incremento 
+++a;
+console.log(a);
+
+// Post-incremento
+a++;
+console.log(a);
+
+//Pre-decremento
+--a;
+console.log(a);
+
+//Post-decremento
+a--;
+console.log(a);
+
+//Ejemplo
+a = 5;
+b = 2;
+c = ++a * b--;
+console.log(c);
+console.log(b);
+console.log(a);
